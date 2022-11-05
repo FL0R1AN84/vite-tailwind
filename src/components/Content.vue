e1988207a0a6b5e5c664ee2d8c34019b26ec7e2f
Florian Wächter <waechter@standpunkt.com>
2022-11-05 16:05:59 +0100

    refactor: 💡 Add a card with close button

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -2,11 +2,34 @@
 </script>
 
 <template>
-  <div class="flex justify-around">
-    <div data-theme="light" class="m-4 p-4">
-      This div will always use light theme
-      <span data-theme="dark">This span will always use dark theme!</span>
+  <div class="flex justify-around items-center">
+    <div class="card w-96 bg-base-100 shadow-xl m-4">
+      <div class="card-body">
+        <div class="card-actions justify-end">
+          <button class="btn btn-square btn-sm">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+        <div data-theme="light" class="m-4 p-4">
+          This div will always use light theme
+          <span data-theme="dark">This span will always use dark theme!</span>
+        </div>
+      </div>
     </div>
+
     <div>
       <p class="text-center text-lg">Good or Bad?</p>
       <label class="swap swap-flip text-9xl">
