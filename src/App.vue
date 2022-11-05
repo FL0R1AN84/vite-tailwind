481468659e7d6431e7af03d1bde39fec46ca0d15
Florian Wächter <waechter@standpunkt.com>
2022-11-05 15:56:31 +0100

    feat: 🎸 Move part of the app to another component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,12 @@
 <script setup>
 import Header from "./components/Header.vue";
+import Content from "./components/Content.vue";
 import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <Header msg="This is my daisyUI playground :-)" />
-  <div class="flex justify-around">
-    <div data-theme="light" class="m-4 p-4">
-      This div will always use light theme
-      <span data-theme="dark">This span will always use dark theme!</span>
-    </div>
-    <div>
-      <p class="text-center text-lg">Good or Bad?</p>
-      <label class="swap swap-flip text-9xl">
-        <input type="checkbox" />
-        <div class="swap-on">😈</div>
-        <div class="swap-off">😇</div>
-      </label>
-    </div>
-  </div>
+  <Content />
   <Footer msg="I´m the Footer" />
 </template>
 
