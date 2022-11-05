efffc81c6cc437dfde462f019b136d76f4cfa158
Florian Wächter <waechter@standpunkt.com>
2022-11-05 16:51:32 +0100

    refactor: 💡 Playing with dark and light theme property

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,31 +1,27 @@
 <script setup>
+import { ref } from 'vue';
+
+const changetext = ref(true);
 </script>
 
 <template>
   <div class="flex justify-around items-center">
-    <div class="card w-96 bg-base-100 shadow-xl m-4">
+    <div class="card w-auto bg-base-100 shadow-xl m-4">
       <div class="card-body">
         <div class="card-actions justify-end">
-          <button class="btn btn-square btn-sm">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
+          <button @click="changetext = !changetext" class="btn btn-square btn-sm">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
         </div>
-        <div data-theme="light" class="m-4 p-4">
+        <div v-if="changetext" data-theme="light" class="m-4 p-4">
           This div will always use light theme
           <span data-theme="dark">This span will always use dark theme!</span>
+        </div>
+        <div v-if="!changetext" data-theme="dark" class="m-4 p-4">
+          This div will always use dark theme
+          <span data-theme="light">This span will always use light theme!</span>
         </div>
       </div>
     </div>
@@ -42,4 +38,5 @@
 </template>
 
 <style scoped>
+
 </style>
